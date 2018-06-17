49ea74e810baac5f27d2ed4953fc747c745fb775
Marius Wilms <mariuswilms@mailbox.org>
2018-06-17 21:05:36 +0200

    Fix schema

--- data/schema.sql
+++ data/schema.sql
@@ -1,3 +1,2 @@
 ALTER TABLE `users` ADD `can_rent` TINYINT(1)  NOT NULL  DEFAULT '1'  COMMENT 'ecommerce'  AFTER `has_accepted_terms`;
-ALTER TABLE `virtual_users` ADD `can_rent` TINYINT(1)  NOT NULL  DEFAULT '1'  COMMENT 'ecommerce'  AFTER `has_accepted_terms`;
 
